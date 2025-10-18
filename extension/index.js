1d8470bece7544b1af0c30643121da045fb2573f
KinzyKenzie <mschoon96@hotmail.com>
2025-10-18 08:08:37 +0200

    Fixed donation tracker issue

--- extension/index.js
+++ extension/index.js
@@ -4,11 +4,9 @@
 const baseUrl = 'https://mcrae.myspeed.run/tracker/api/v2';
 const eventId = 4;
 
-// https://www.nodecg.dev/docs/extensions
 module.exports = function(nodecg) {
-    // https://www.nodecg.dev/docs/classes/replicant
     // Declare this replicant at start up to make sure there's always a default if needed.
-    const donationTotal = nodecg.Replicant('donationTotal', { default: {} });
+    const donationData = nodecg.Replicant('donationData', { default: {} });
 
     // Function to be triggered when needing to update the donation total value.
     const updateDonationTotalFromApi = async function () {
@@ -16,7 +14,10 @@ module.exports = function(nodecg) {
         if (resp.ok) {
             const data = await resp.json();
             if (data) {
-                donationTotal.value = data;
+                if (donationData.value != data.amount) {
+                    nodecg.log.info(`Donation total updated: ${data.amount}`);
+                }
+                donationData.value = data;
             } else {
                 nodecg.log.info(`ERROR: ${resp.statusText}`);
             }
@@ -24,6 +25,7 @@ module.exports = function(nodecg) {
             nodecg.log.info(`ERROR: ${resp.statusText}`);
         }
     }
-    setTimeout(() => { updateDonationTotalFromApi() }, 20 * 1000); // run every 20 seconds
-    updateDonationTotalFromApi(); // also run at startup
+    
+    updateDonationTotalFromApi(); // run at startup
+    setInterval(() => { updateDonationTotalFromApi() }, 58 * 1000); // also run every minute
 };
--- graphics/js/donations.js
+++ graphics/js/donations.js
@@ -1,7 +1,7 @@
-const donationTotal = nodecg.Replicant('donationTotal', { default: {} });
+const donationData = nodecg.Replicant('donationData', { default: {} });
 const readout =  document.getElementById('donation-total');
 
-donationTotal.on('change', (newVal, oldVal) => {
+donationData.on('change', (newVal, oldVal) => {
     readout.innerHTML = new Intl.NumberFormat("en-GB", { style: "currency", currency: newVal.paypalcurrency })
         .format( newVal.amount );
 });
