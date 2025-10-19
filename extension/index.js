2a0a45963d16fd2dcd3a786cb825720dcf8c9558
KinzyKenzie <mschoon96@hotmail.com>
2025-10-19 14:03:28 +0200

    started implementing config data

--- extension/index.js
+++ extension/index.js
@@ -1,28 +1,29 @@
-// Donation code is similar to uksg-layouts: https://github.com/uksgmarathon/uksg-layouts/blob/main/src/extension/tracker.ts
-
-// Could put these in a configuration file.
-const baseUrl = 'https://mcrae.myspeed.run/tracker/api/v2';
-const eventId = 4;
-
 module.exports = function(nodecg) {
-    // Declare this replicant at start up to make sure there's always a default if needed.
     const donationData = nodecg.Replicant('donationData', { default: {} });
+    const config = nodecg.bundleConfig.tracker ?? { enabled: false };
 
-    // Function to be triggered when needing to update the donation total value.
-    const updateDonationTotalFromApi = async function () {
-        const resp = await fetch(`${baseUrl}/events/${eventId}?totals=true&format=json`);
+    const fetchDonationTotals = async function () {
+        if(!config.enabled) { return; }
+
+        const resp = await fetch(`${config.baseUrl}/events/${config.eventId}?totals=true&format=json`);
+        let success = false;
+
         if (resp.ok) {
             const data = await resp.json();
             if (data) {
                 donationData.value = data;
+                success = true;
             } else {
-                nodecg.log.info(`ERROR: ${resp.statusText}`);
+                nodecg.log.info(`[Tracker] ERROR: ${resp.statusText}`);
             }
         } else {
-            nodecg.log.info(`ERROR: ${resp.statusText}`);
+            nodecg.log.info(`[Tracker] ERROR: ${resp.statusText}`);
         }
+        return success;
     }
     
-    updateDonationTotalFromApi(); // run at startup
-    setInterval(() => { updateDonationTotalFromApi() }, 58 * 1000); // also run every minute
+    if (fetchDonationTotals()) {
+        nodecg.log.info(`[Tracker] Successfully connected to API endpoint`);
+    }
+    setInterval(() => { fetchDonationTotals() }, 58 * 1000);
 };
